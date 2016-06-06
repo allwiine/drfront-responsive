a19885fe6899c5d7cf0ca1ced0142396ec8b90b2
glenn <glenn.abra@gmail.com>
2016-06-06 17:01:45 +0200

    Add sass-lint to build.

--- gulp/config.js
+++ gulp/config.js
@@ -5,8 +5,9 @@ module.exports = {
     src: src,
     dest: dest,
     sass: {
+    	default: src + "/sass/default.scss",
         src: [
-            src + "/sass/default.scss"
+            src + "/sass/**/*.s+(a|c)ss"
         ],
         name: "drfront-responsive.scss",
         dest: dest + "/css"
